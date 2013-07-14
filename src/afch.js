f41e234fac4373732cfb1797d15d6cc73d66bfc5
Mabdul <mabdul@blah.ch>
2013-07-14 13:59:44 +0200

    Enable review tab on all pages in userspace for easy sandbox reviewing as reported at [[WP:AFCH/DEV]]

--- src/afch.js
+++ src/afch.js
@@ -23,7 +23,11 @@ if ((wgPageName.indexOf('Wikipedia:Articles_for_creation') !== -1) || (wgPageNam
 
 	if (wgPageName.indexOf('Wikipedia:Articles_for_creation/Redirects') !== -1) {
 		importScriptURI(afchelper_baseurl + '/redirects.js');
-	} else if (wgPageName.indexOf('Wikipedia:Articles_for_creation/') !== -1 || wgPageName.indexOf('Wikipedia_talk:Articles_for_creation/') !== -1) {
+	} else if ((wgPageName.indexOf('Wikipedia:Articles_for_creation/') !== -1)
+	            || (wgPageName.indexOf('Wikipedia_talk:Articles_for_creation/') !== -1)
+	            || (wgPageName.indexOf('User:') !== -1)
+	            || (wgPageName.indexOf('User_talk:') !== -1)
+	            ){
 		importScriptURI(afchelper_baseurl + '/submissions.js');				
 	}
 }
