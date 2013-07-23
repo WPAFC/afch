04c2ce19235ee60d2842f7c5ff4464235974cd14
theopolisme <theopolismewiki@gmail.com>
2013-07-22 21:46:14 -0500

    Fix YAT*
    *yet another typo

--- src/afch.js
+++ src/afch.js
@@ -19,7 +19,7 @@ if ((wgPageName.indexOf('Wikipedia:Articles_for_creation') !== -1) || (wgPageNam
 
 	if (wgPageName.indexOf('Wikipedia:Articles_for_creation/Redirects') !== -1) {
 		importScriptURI(afchelper_baseurl + '/redirects.js');
-	} else if ((wgPageName.indexOf('Wikipedia:Files_for_upload') !== -1) {
+	} else if (wgPageName.indexOf('Wikipedia:Files_for_upload') !== -1) {
 		importScriptURI(afchelper_baseurl + '/ffu.js');		
 	} else if ((wgPageName.indexOf('Wikipedia:Articles_for_creation/') !== -1)
 				|| (wgPageName.indexOf('Wikipedia_talk:Articles_for_creation/') !== -1)
