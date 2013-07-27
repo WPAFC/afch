94e9e05630c321993ab50e254b1a9651eb48a11c
Mabdul <mabdul@blah.ch>
2013-07-27 14:21:23 +0200

    fixing Problem with oading subscripts (reported as IE8/9 problem) fix #64 - although this problem is a general problem!

--- src/afch.js
+++ src/afch.js
@@ -7,7 +7,7 @@
 //<nowiki>
 if ((wgPageName.indexOf('Wikipedia:Articles_for_creation') !== -1) || (wgPageName.indexOf('Wikipedia_talk:Articles_for_creation') !== -1) || (wgPageName.indexOf('Wikipedia:Files_for_upload') !== -1) || (wgPageName.indexOf('User:*') !== -1)) {
 	importScript('User:Timotheus Canens/displaymessage.js');
-	var afchelper_baseurl = '//' + mw.config.get('wgServer') + '/w/index.php?action=raw&ctype=text/javascript&title=MediaWiki:Gadget-afchelper.js';
+	var afchelper_baseurl = mw.config.get('wgServer') + '/w/index.php?action=raw&ctype=text/javascript&title=MediaWiki:Gadget-afchelper.js';
 	importScriptURI(afchelper_baseurl + '/core.js');
 	var afcHelper_advert = ' ([[WP:AFCH|AFCH]])';
 	var pagetext = '';
