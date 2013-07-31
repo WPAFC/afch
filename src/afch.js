d7adbf8033f3028010d8985a6abafb14ea674aca
theopolisme <theopolismewiki@gmail.com>
2013-07-30 23:22:44 -0500

    Move script to core.js, since apparently gadgets work in a separate space than the rest of the javascript, which was causing resource errors

--- src/afch.js
+++ src/afch.js
@@ -6,23 +6,7 @@
 ////////////////////////////////////////////
 //<nowiki>
 if ((wgPageName.indexOf('Wikipedia:Articles_for_creation') !== -1) || (wgPageName.indexOf('Wikipedia_talk:Articles_for_creation') !== -1) || (wgPageName.indexOf('Wikipedia:Files_for_upload') !== -1) || (wgPageName.indexOf('User:*') !== -1)) {
-	importScript('User:Timotheus Canens/displaymessage.js');
 	var afchelper_baseurl = mw.config.get('wgServer') + '/w/index.php?action=raw&ctype=text/javascript&title=MediaWiki:Gadget-afchelper.js';
 	importScriptURI(afchelper_baseurl + '/core.js');
-	var afcHelper_advert = ' ([[WP:AFCH|AFCH]])';
-	var pagetext = '';
-	var usertalkpage = '';
-
-	if (wgPageName.indexOf('Wikipedia:Articles_for_creation/Redirects') !== -1) {
-		importScriptURI(afchelper_baseurl + '/redirects.js');
-	} else if (wgPageName.indexOf('Wikipedia:Files_for_upload') !== -1) {
-		importScriptURI(afchelper_baseurl + '/ffu.js');		
-	} else if ((wgPageName.indexOf('Wikipedia:Articles_for_creation/') !== -1)
-				|| (wgPageName.indexOf('Wikipedia_talk:Articles_for_creation/') !== -1)
-				|| (wgPageName.indexOf('User:') !== -1)
-				|| (wgPageName.indexOf('User_talk:') !== -1)
-				){
-		importScriptURI(afchelper_baseurl + '/submissions.js');				
-	}
 }
 //</nowiki>
