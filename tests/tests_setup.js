35671f9760b25dada585e878678c9e85971d655c
theopolisme <theopolismewiki@gmail.com>
2013-08-25 11:17:03 -0500

    mess around some more…still not really functional, but getting there. qunit is fun.

--- tests/tests_setup.js
+++ tests/tests_setup.js
@@ -1,8 +1,4 @@
 $("#bodyContent").prepend('<div id="qunit"></div><div id="qunit-fixture"></div>')
 $('head').append('<link rel="stylesheet" type="text/css" href="//code.jquery.com/qunit/qunit-1.12.0.css">');
-
-$.ajax({
-	url: '//code.jquery.com/qunit/qunit-1.12.0.js',
-	dataType: "script",
-	async: false
-});
+$('head').append('<script type="text/javascript" src="//code.jquery.com/qunit/qunit-1.12.0.js">');
+$('head').append('<script type="text/javascript" src="//raw.github.com/WPAFC/afch/feature-unittests/tests/tests.js">');
