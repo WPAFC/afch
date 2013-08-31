38085a873cee7c20e5ba34745ab8c38349fcb44c
theopolisme <theopolismewiki@gmail.com>
2013-08-30 20:14:28 -0500

    Add afcHelper_expand class and make textareas not expand until clicked in

--- src/submissions.js
+++ src/submissions.js
@@ -323,13 +323,12 @@ function afcHelper_prompt(type) {
 			success: function (data) { afcHelper_wikiprojectindex = data; },
 			async: false
 		});
-
 		// Then generate a dynamic menu for them
-		var wikiprojectSelect = afcHelper_generateChzn("afcHelper_wikiproject_selection",'Start typing...',afcHelper_wikiprojectindex);
-		text += '<br /><label for="afcHelper_wikiproject_selection">Choose associated WikiProjects to be added to the talk page: </label>' + wikiprojectSelect;
-		text += '<br /><label for="afcHelper_pagePrepend">Prepend wikicode to page (optional, e.g. maintenance boxes): </label><textarea rows="2" cols="60" id="afcHelper_pagePrepend"></textarea>' +
-		'<br /><label for="afcHelper_pageAppend">Append wikicode to page (optional, e.g. categories or stub templates): </label><textarea rows="2" cols="60" id="afcHelper_pageAppend"></textarea>' +
-		'<br /><label for="afcHelper_talkAppend">Append wikicode to talk page (optional, e.g. WikiProject templates): </label><textarea rows="2" cols="60" id="afcHelper_talkAppend"></textarea>' +
+		var wikiprojectSelect = afcHelper_generateChzn("afcHelper_wikiproject_selection",'Start typing the name of a related topic...',afcHelper_wikiprojectindex);
+		text += '<br /><label for="afcHelper_wikiproject_selection">Choose associated WikiProjects to be automatically be added to the talk page: </label><br>' + wikiprojectSelect;
+		text += '<br /><label for="afcHelper_pagePrepend">Prepend wikicode to page (optional, e.g. maintenance boxes): </label><br><textarea class="afcHelper_expand" rows="1" cols="60" id="afcHelper_pagePrepend"></textarea>' +
+		'<br /><label for="afcHelper_pageAppend">Append wikicode to page (optional, e.g. categories or stub templates): </label><br><textarea class="afcHelper_expand" rows="1" cols="60" id="afcHelper_pageAppend"></textarea>' +
+		'<br /><label for="afcHelper_talkAppend">Append wikicode to talk page (optional, e.g. WikiProject templates): </label><br><textarea class="afcHelper_expand" rows="1" cols="60" id="afcHelper_talkAppend"></textarea>' +
 		'<br /><label for="afcHelper_reqphoto">Does the article need a photo/image? (add &#123;&#123;<a href="'+ wgArticlePath.replace("$1", 'Template:Reqphoto') + '" title="Template:Reqphoto" target="_blank">reqphoto</a>&#125;&#125; to talk page) </label><input type="checkbox" id="afcHelper_reqphoto"/>' +
 		'<br /><label for="afcHelper_reqinfobox">Does the article need an infobox? (add &#123;&#123;<a href="'+ wgArticlePath.replace("$1", 'Template:Reqinfobox') + '" title="Template:Reqinfobox" target="_blank">reqinfobox</a>&#125;&#125; to talk page) </label><input type="checkbox" id="afcHelper_reqinfobox"/>' +
 		'<br /><label for="afcHelper_biography">Is the article a biography? </label><input type="checkbox" id="afcHelper_biography" onchange=afcHelper_trigger(\'afcHelper_biography_blp\') />' +
@@ -355,7 +354,12 @@ function afcHelper_prompt(type) {
 		'</div></div><div id="afcHelper_extra_inline"></div>' +
 		'<button class="afcHelper_button" type="button" id="afcHelper_accept_button" onclick="afcHelper_act(\'accept\')">Accept and publish to mainspace</button>';
 		$("#afcHelper_extra").html(text);
+		// Set up chosen wikiproject menu
 		$("#afcHelper_wikiproject_selection").chosen({no_results_text: "Oops, couldn't find any WikiProjects matching your input!"}); 
+		// Expand textareas on click so they don't take up space
+		$('textarea.afcHelper_expand').focus(function () {
+			$(this).animate({ height: "4em" }, 500);
+		});
 	} else if (type === 'decline') {
 		var text = '<h3>Declining ' + afcHelper_PageName + '</h3>' + '<label for="afcHelper_reason">Reason for ' + type + ': </label>';
 		var reasonSelect = afcHelper_generateSelect("afcHelper_reason", afcHelper_reasonhash, "afcHelper_onChange(this)");
