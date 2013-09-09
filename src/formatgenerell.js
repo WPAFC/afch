2e97add6851d50ecfd282591a748716599a56302
wikipedia-mabdul <mabdul@blah.ch>
2013-09-09 14:47:15 +0200

    Rename submissions.js to formatgenerell.js

--- src/formatgenerell.js
+++ src/formatgenerell.js
@@ -603,7 +603,7 @@ function afcHelper_act(action) {
 			var lastentry = recenttext.toLowerCase().lastIndexOf("\{\{afc contrib");
 			var firstentry = recenttext.toLowerCase().indexOf("\{\{afc contrib");
 			recenttext = recenttext.substring(0, lastentry);
-			recenttext = recenttext.substring(0, firstentry) + newentry + recenttext.substring(firstentry);
+			recenttext = recenttext.substring(0, firstentry) + newentry + recenttext.substring(firstenafcHelper_talkAppendtry);
 			afcHelper_editPage("Wikipedia:Articles for creation/recent", recenttext, 'Updating recent AFC creations', false);
 
 			var talktext = "";
