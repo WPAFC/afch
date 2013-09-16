e139cacf872fe8c5708e21940e1f4d19b80cf0e9
theopolisme <theopolismewiki@gmail.com>
2013-09-16 16:51:07 -0500

    Better "uh-oh!" message

--- src/submissions.js
+++ src/submissions.js
@@ -191,7 +191,7 @@ function afcHelper_init() {
 	displayMessage('<div id="afcHelper_loadingmsg">Loading the Article for creation helper script...</div>');
 
 	if (!wfSupportsAjax()) {
-		displayMessage('<span class="notice">Uh oh. Your browser appears to be too old to handle this script or does not support AJAX. Please use the latest version of Mozilla Firefox, Apple Safari, Google Chrome, or Opera for best results. Sorry about that.</span>');
+		displayMessage('<span class="notice">Uh-oh! Your browser appears to be too old to handle the helper script or does not support AJAX. Please <a href="http://browsehappy.com">upgrade</a> to the latest version of Firefox, Safari, Chrome, or Opera for best results. Sorry about that; please <a href="https://en.wikipedia.org/wiki/Wikipedia_talk:WikiProject_Articles_for_creation/Helper_script" target="_blank">let us know</a> if you need further assistance.</span>');
 		return;
 	}
 
