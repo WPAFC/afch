b90b78c9b66c6e3ad9c879fa45b8de179142b96d
theopolisme <theopolismewiki@gmail.com>
2013-09-28 23:14:33 -0500

    Stop removing so many newlines
    See bad
    https://test.wikipedia.org/w/index.php?title=Wikipedia_talk:Articles_for
    _creation/theo_sandbox&diff=180691&oldid=180690 versus good
    https://test.wikipedia.org/w/index.php?title=Wikipedia_talk%3AArticles_f
    or_creation%2Ftheo_sandbox&diff=180700&oldid=180699

--- src/submissions.js
+++ src/submissions.js
@@ -1157,10 +1157,10 @@ function afcHelper_cleanup(text,type) {
 	}
 
 	//Ref tag correction part #1: remove whitespaces and commas between the ref tags and whitespaces before ref tags
-	text = text.replace(/\s*(\<\/\s*ref\s*\>)\s*[,]*\s*(<\s*ref\s*(name\s*=|group\s*=)*\s*[^\/]*>)\s*$/gim, "$1$2");
-	text = text.replace(/\s*(<\s*ref\s*(name\s*=|group\s*=)*\s*.*[^\/]+>)\s*$/gim, "$1");
+	text = text.replace(/\s*(\<\/\s*ref\s*\>)\s*[,]*\s*(<\s*ref\s*(name\s*=|group\s*=)*\s*[^\/]*>)[ \t]*$/gim, "$1$2");
+	text = text.replace(/\s*(<\s*ref\s*(name\s*=|group\s*=)*\s*.*[^\/]+>)[ \t]*$/gim, "$1");
 	//Ref tag correction part #2: move :;.,!? before ref tags
-	text = text.replace(/\s*((<\s*ref\s*(name\s*=|group\s*=)*\s*.*[\/]{1}>)|(<\s*ref\s*(name\s*=|group\s*=)*\s*[^\/]*>(?:\\<[^\<\>]*\>|[^><])*\<\/\s*ref\s*\>))\s*([.!?,;:])+$/gim, "$6$1");
+	text = text.replace(/\s*((<\s*ref\s*(name\s*=|group\s*=)*\s*.*[\/]{1}>)|(<\s*ref\s*(name\s*=|group\s*=)*\s*[^\/]*>(?:\\<[^\<\>]*\>|[^><])*\<\/\s*ref\s*\>))[ \t]*([.!?,;:])+$/gim, "$6$1");
 
 	// Remove all unneeded HTML comments and wizards stuff
 	text = text.replace("\* \[http\:\/\/www.example.com\/ example.com\]", "");
