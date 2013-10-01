6141c71e1a2ce1f09a209d9d509195d59888d4ee
Donald J. Fortier II <Technical_13@yahoo.com>
2013-10-01 14:32:24 -0400

    Update submissions.js
    Fix for [14:09:36.460] ReferenceError: assignment to undeclared variable warnings @ https://en.wikipedia.org/w/index.php?action=raw&ctype=text/javascript&title=User:Theo%27s%20Little%20Bot/afch/afchelper.js/submissions.js:1347

--- src/submissions.js
+++ src/submissions.js
@@ -1344,7 +1344,7 @@ function afcHelper_setup() {
 	pagetext = pagetext.replace(/\{\{\s*AFC submission(\s*\|){0,}ts\s*=\s*/gi, "{{AFC submission|||ts=");
 	pagetext = pagetext.replace(/\{\{\s*AFC submission\s*\}\}/gi, "{{AFC submission|||ts=99999999999999|u=Example|ns="+ wgNamespaceNumber + "}}");
 	pagetext = afcHelper_cleanup(pagetext,'initial');
-	warnings = afcHelper_warnings(pagetext); // Warn about problems with given pagetext
+	var warnings = afcHelper_warnings(pagetext); // Warn about problems with given pagetext
 
 	return warnings; // Prepends the warnings
 }
