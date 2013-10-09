9fe86d482f37e81882474611cd968b4014627ec7
theopolisme <theopolismewiki@gmail.com>
2013-10-09 12:52:31 -0500

    Remove apparently broken AutoEd implementation as well
    importScriptURI is asynchronous, which means the code doesn't wait for
    the scripts to load before continuing, resulting in reference errors
    (the reason I didn't see them is becuause I had AutoEd installed
    already as a userscript, so it was pre-loading it). @wikipedia-mabdul
    we need to get away from importScriptURI… and this needs to be figured
    out by next release.

--- src/submissions.js
+++ src/submissions.js
@@ -1090,40 +1090,6 @@ function afcHelper_cleanup(text,type) {
 		text = text.replace(/\[\[Category:/gi, "\[\[:Category:");
 	}
 
-	if (type !== 'initial') {
-		/* Don't run external cleanup scripts on initial load to speed up opening */
-		// Run AutoEd automatically
-		var AutoEd_baseurl = '//en.wikipedia.org/w/index.php?action=raw&ctype=text/javascript&title=Wikipedia:AutoEd/';
-		//importScriptURI(AutoEd_baseurl + 'core.js'); //Imports the "framework" script needed to make this function
-		
-		//Import individual modules for use
-		importScriptURI(AutoEd_baseurl + 'unicodify.js'); // autoEdUnicodify() converts HTML entities to WikiText
-		importScriptURI(AutoEd_baseurl + 'isbn.js'); // autoEdISBN() fixes ISBN syntax so that WikiMagic can work
-		importScriptURI(AutoEd_baseurl + 'whitespace.js'); // autoEdWhitespace() cleans up whitespace
-		importScriptURI(AutoEd_baseurl + 'wikilinks.js'); // autoEdWikilinks() simplifies and shortens wikilinks where appropriate
-		importScriptURI(AutoEd_baseurl + 'htmltowikitext.js'); // autoEdHTMLtoWikitext() converts HTML to wikitext
-		importScriptURI(AutoEd_baseurl + 'headlines.js'); // autoEdHeadlines() fixes common headline errors and renames some headers
-		importScriptURI(AutoEd_baseurl + 'unicodecontrolchars.js'); // autoEdUnicodeControlChars() converts HTML to wikitext
-		importScriptURI(AutoEd_baseurl + 'unicodehex.js'); // autoEdUnicodeHex() converts hex encoded characters to unicode
-		importScriptURI(AutoEd_baseurl + 'templates.js'); // autoEdTemplates() cleans up templates
-		importScriptURI(AutoEd_baseurl + 'tablestowikitext.js'); // autoEdTablestoWikitext() replaces HTML tables with wikitables
-		importScriptURI(AutoEd_baseurl + 'extrabreaks.js'); // autoEdExtraBreaks() removes extra BR tags
-		importScriptURI(AutoEd_baseurl + 'links.js'); // autoEdLinks() cleans up common link errors
-
-		text = autoEdUnicodify(text);
-		text = autoEdISBN(text);
-		text = autoEdWhitespace(text);
-		text = autoEdUnicodeHex(text);
-		text = autoEdWikilinks(text);
-		text = autoEdHTMLtoWikitext(text);
-		text = autoEdHeadlines(text);
-		text = autoEdUnicodeControlChars(text);
-		text = autoEdTemplates(text);
-		text = autoEdTablestoWikitext(text);
-		text = autoEdExtraBreaks(text);
-		text = autoEdLinks(text);
-	}
-
 	//Ref tag correction part #1: remove whitespaces and commas between the ref tags and whitespaces before ref tags
 	text = text.replace(/\s*(\<\/\s*ref\s*\>)\s*[,]*\s*(<\s*ref\s*(name\s*=|group\s*=)*\s*[^\/]*>)[ \t]*$/gim, "$1$2");
 	text = text.replace(/\s*(<\s*ref\s*(name\s*=|group\s*=)*\s*.*[^\/]+>)[ \t]*$/gim, "$1");
